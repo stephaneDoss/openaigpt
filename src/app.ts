03b2f6d011e0e939a4b5fe8ab46fb9b0bf3e39d9
Stephane Dossou <sedegnon.dossou@epitech.eu>
2022-12-25 07:36:14 +0100

    Projet modifié en synonime et antonyme

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ const submitButton = document.querySelector<HTMLButtonElement>("button");
 const footer = document.querySelector<HTMLElement>("footer");
 
 const token = "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";
-const generatePromptByAgeAndThemes = (age: number, themes = "") => {
-  let prompt = `Propose moi, avec un ton joyeux et amical, 5 idées de cadeau pour une personne âgée de ${age} ans`;
+const generatePromptByAgeAndThemes = (choix: string, mot = "") => {
+  // let prompt = `Propose moi, avec un ton joyeux et amical, 5 idées de cadeau pour une personne âgée de ${age} ans`;
+  let prompt = `Donne moi quelques exemples de ${choix} du mot ${mot}`;
 
-  if (themes.trim()) {
-    prompt += ` et qui aime ${themes}`;
+  if (!mot.trim()) {
+    // prompt += ` et qui aime ${themes}`;
+    console.log("erreur");
   }
   return prompt + " !";
 };
 
 const setLoadingItems = () => {
-  footer!.textContent = "Chargement de super idées en cours !";
+  footer!.textContent = "Chargement de mots en cours !";
   footer!.setAttribute("aria-busy", "true");
   submitButton?.setAttribute("aria-busy", "true");
   submitButton!.disabled = true;
@@ -30,6 +32,10 @@ const removeLoadingItems = () => {
 form?.addEventListener("submit", (e: SubmitEvent) => {
   e.preventDefault();
   setLoadingItems();
+  console.log(
+    generatePromptByAgeAndThemes(ageInput!.value, themesInput?.value)
+  );
+
   fetch(`https://api.openai.com/v1/completions`, {
     method: "POST",
     headers: {
@@ -37,10 +43,7 @@ form?.addEventListener("submit", (e: SubmitEvent) => {
       Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
-      prompt: generatePromptByAgeAndThemes(
-        ageInput!.valueAsNumber,
-        themesInput?.value
-      ),
+      prompt: generatePromptByAgeAndThemes(ageInput!.value, themesInput?.value),
       max_tokens: 2000,
       model: "text-davinci-003",
     }),
